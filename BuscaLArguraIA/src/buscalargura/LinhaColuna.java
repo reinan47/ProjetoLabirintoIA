59efd4a4c09cf676fe77080a33d080f8f392c701
Reinan <reinanzinho100@gmail.com>
2022-08-27 22:08:03 -0300

    Adicionado a Classe LinhaColuna.java. resolve #1

---
+++ BuscaLArguraIA/src/buscalargura/LinhaColuna.java
@@ -0,0 +1,22 @@
+package buscalargura;
+
+/**
+ *
+ * @author Reinan
+ */
+public class LinhaColuna {
+    private int linha;
+    private int coluna;
+    public LinhaColuna(int linha, int coluna){
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+}

ac19ae187caa89b916ba7d692b1bd800dee1875b
Reinan <reinanzinho100@gmail.com>
2022-08-29 10:08:59 -0300

    Mudanças feitas na Interface/Painel.java. resolve #5

--- BuscaLArguraIA/src/Interface/Painel.java
+++ BuscaLArguraIA/src/Interface/Painel.java
@@ -1,31 +1,24 @@
 package Interface;
 
-import java.awt.Color;
 import java.util.concurrent.TimeUnit;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import buscalargura.BuscaLargura;
 import buscalargura.LinhaColuna;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.SystemColor;
 import javax.swing.JLabel;
+
 import javax.swing.JPanel;
 
 /**
  *
  * @author Reinan
  */
-public class Painel extends JFrame{
-    JLabel jl = new JLabel("BUSCA EM LARGURA");
+public class Painel extends JPanel{
     public Painel(){
         this.setLayout(null);
-        this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1000,700);
-        JPanel p = new JPanel();
-        p.setLayout(null);
-        p.setBackground(Color.YELLOW);
-        setContentPane(p); 
+        this.setBackground(SystemColor.YELLOW);
     }
    
     
@@ -34,11 +27,21 @@ public class Painel extends JFrame{
         JButton origem = new JButton();
         JButton []parede = new JButton[130];
         BuscaLargura bl = new BuscaLargura();
+        JLabel titulo = new JLabel("BUSCA EM LARGURA");
         LinhaColuna[] caminho = (LinhaColuna[]) bl.BuscaLargura();
         
         Painel p = new Painel();
-        p.setVisible(true);
-  
+        
+        JFrame ja = new JFrame("");
+        ja.setBounds(0, 0, 1000, 700);
+        ja.setLocationRelativeTo(null);
+        ja.getContentPane().add(p);
+        ja.setVisible(true);
+        ja.setDefaultCloseOperation(ja.EXIT_ON_CLOSE);
+        
+ 
+        titulo.setBounds(420, 30, 200, 30);
+        p.add(titulo);
         int cont = 0;
         for(int i = 0 ; i < 13 ; i++){
             for(int j = 0 ; j < 17 ; j++){
@@ -53,19 +56,26 @@ public class Painel extends JFrame{
                 if(bl.tabuleiro[i][j] == 0){
                     
                     parede[cont].setBounds(75+(j*50), 100+(i*30), 50, 30);
-                    TimeUnit.MILLISECONDS.sleep(10);
-                    parede[cont].setBackground(Color.GREEN);
-                    p.setContentPane(parede[cont]);
+                    parede[cont].setBackground(SystemColor.GREEN);
+                    parede[cont].setBorderPainted(false);
+                    p.add(parede[cont]);
+                    p.revalidate();
+                    ja.repaint();
+                    //p.setContentPane(parede[cont]);
                     cont++;    
                 }
             }
         }
         for(int i = 0 ; i < 110 ; i++){
             if(caminho[i] != null){
-                origem.setBackground(Color.BLACK);
+                origem.setBackground(SystemColor.BLACK);
                 origem.setBounds(75+(caminho[i].getColuna()*50), 100+(caminho[i].getLinha()*30), 50, 30);
                 TimeUnit.MILLISECONDS.sleep(200);
-                p.setContentPane(origem);
+                origem.setBorderPainted(false);
+                p.add(origem);
+                p.revalidate();
+                ja.repaint();
+               // p.setContentPane(origem);
             }else{
                 break;
             }

301f8caec5358b5bf03cdcc95b5be7a0a277d059
Reinan <reinanzinho100@gmail.com>
2022-09-01 22:58:54 -0300

    adicionando o projeto com as movimentações finais. resolve #6

--- BuscaLArguraIA/src/Interface/Painel.java
+++ BuscaLArguraIA/src/Interface/Painel.java
@@ -6,7 +6,9 @@ import javax.swing.JFrame;
 import buscalargura.BuscaLargura;
 import buscalargura.LinhaColuna;
 import java.awt.SystemColor;
+import static java.lang.System.exit;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import javax.swing.JPanel;
 
@@ -15,24 +17,36 @@ import javax.swing.JPanel;
  * @author Reinan
  */
 public class Painel extends JPanel{
+    JButton b = new JButton("SAIR");
     public Painel(){
         this.setLayout(null);
         this.setSize(1000,700);
         this.setBackground(SystemColor.YELLOW);
+        b.setBounds(450, 600, 100, 40);
+        b.setBackground(SystemColor.LIGHT_GRAY);
+        b.setBorderPainted(false);
+        add(b);
+        
+        
+        b.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                System.exit(0);
+            }
+        });
     }
    
-    
-    
     public static void main(String[] args) throws InterruptedException{
         JButton origem = new JButton();
-        JButton []parede = new JButton[130];
+        JButton []parede = new JButton[250];
         BuscaLargura bl = new BuscaLargura();
         JLabel titulo = new JLabel("BUSCA EM LARGURA");
-        LinhaColuna[] caminho = (LinhaColuna[]) bl.BuscaLargura();
-        
+        LinhaColuna[] caminho = bl.BuscaLargura();
+ 
         Painel p = new Painel();
-        
         JFrame ja = new JFrame("");
+        
+        
+        p.setBounds(0, 0, 50, 800);
         ja.setBounds(0, 0, 1000, 700);
         ja.setLocationRelativeTo(null);
         ja.getContentPane().add(p);
@@ -40,10 +54,11 @@ public class Painel extends JPanel{
         ja.setDefaultCloseOperation(ja.EXIT_ON_CLOSE);
         
  
-        titulo.setBounds(420, 30, 200, 30);
+        titulo.setBounds(440, 10, 200, 30);
         p.add(titulo);
+        
         int cont = 0;
-        for(int i = 0 ; i < 13 ; i++){
+        for(int i = 0 ; i < 18 ; i++){
             for(int j = 0 ; j < 17 ; j++){
                 if(bl.tabuleiro[i][j] == 0){
                    parede[cont++] = new JButton();
@@ -51,36 +66,42 @@ public class Painel extends JPanel{
             }
         }
         cont = 0;
-        for(int i = 0 ; i < 13 ; i++){
+        for(int i = 0 ; i < 18 ; i++){
             for(int j = 0 ; j < 17 ; j++){
                 if(bl.tabuleiro[i][j] == 0){
-                    
-                    parede[cont].setBounds(75+(j*50), 100+(i*30), 50, 30);
+                    parede[cont].setBounds(70+(j*50), 40+(i*30), 50, 30);
                     parede[cont].setBackground(SystemColor.GREEN);
                     parede[cont].setBorderPainted(false);
                     p.add(parede[cont]);
                     p.revalidate();
                     ja.repaint();
-                    //p.setContentPane(parede[cont]);
                     cont++;    
                 }
             }
         }
+        
+        boolean aux = true;
         for(int i = 0 ; i < 110 ; i++){
             if(caminho[i] != null){
                 origem.setBackground(SystemColor.BLACK);
-                origem.setBounds(75+(caminho[i].getColuna()*50), 100+(caminho[i].getLinha()*30), 50, 30);
-                TimeUnit.MILLISECONDS.sleep(200);
+                origem.setBounds(70+(caminho[i].getColuna()*50), 40+(caminho[i].getLinha()*30), 50, 30);
+                TimeUnit.MILLISECONDS.sleep(500);
                 origem.setBorderPainted(false);
                 p.add(origem);
                 p.revalidate();
                 ja.repaint();
-               // p.setContentPane(origem);
+                if(aux){
+                    TimeUnit.SECONDS.sleep(2);
+                    aux = false;
+                }     
             }else{
                 break;
             }
             
         }
+        
+        JOptionPane.showMessageDialog(null, "IA achou o caminho.");
+        exit(0);
     }
 }
 
--- BuscaLArguraIA/src/buscalargura/BuscaLargura.java
+++ BuscaLArguraIA/src/buscalargura/BuscaLargura.java
@@ -19,24 +19,29 @@ import javax.swing.JButton;
 public class BuscaLargura extends VerificarVisitados{
     private int boneco;
     private int objetivo;
-    private int [] predecessor = new int [221];
-    private int [] visitados = new int [221];
-    private int [] caminho = new int [110];
+    private int [] predecessor = new int [306];
+    private int [] visitados = new int [306];
+    private int [] caminho = new int [153];
     private int parede = 0;
     public int [][] tabuleiro = {
             {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
             {  0, 19, 20, 21, 22, 23, 24, 25,  26, 27, 28, 29, 0, 31, 32, 33,  0},
             {  0, 36,  0, 38,  0,  0,  0,  0,  0, 44,  0,  0,  0,  0,  0, 50,  0},
             {  0, 52,  0, 54, 55, 56,  0, 58, 59, 60, 61, 62, 63, 64,  0, 66,  0},
-            {  0,  0,  0,  0,  0, 73,  0,  0,  0, 77,  0,  0,  0, 81,  0, 83,  0},
+            {  0,  0,  0,  0,  0, 73,  0,  0,  0,  0,  0,  0,  0, 81,  0, 83,  0},
             {  0, 86, 87, 88, 89, 90,  0, 92, 93, 94,  0, 96, 97, 98, 99,100,  0},
             {  0,103,  0,105,  0,  0,  0,  0,  0,110,  0,112,  0,  0,  0,116,  0},
             {  0,119,  0,121,122,123,124,125,  0,127,128,129,  0,131,132,133,  0},
-            {  0,135,  0,  0,  0,  0,140,  0,  0,143,  0,145,  0,147,  0,  0,  0},
+            {  0,135,  0,  0,  0,  0,140,  0,  0,143,  0,145,  0,  0,  0,  0,  0},
             {  0,152,  0,154,155,156,157,158,159,160,  0,162,163,164,165,166,  0},
-            {  0,168,  0,170,  0,  0,  0,  0,  0,  0,  0,178,  0,  0,  0,182,  0},
-            {  0,185,186,187,  0,189,190,191,192,193,194,195,  0,197,198,199,200},
-            {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}    
+            {  0,168,  0,170,  0,  0,  0,  0,  0,  0,  0,178,  0,  0,  0,  0,  0},
+            {  0,185,186,187,  0,189,190,191,  0,193,194,195,  0,197,198,199,200},
+            {  0,202,  0,  0,  0,206,  0,208,  0,210,  0,  0,  0,  0,  0,216,  0},  
+            {  0,219,  0,221,222,223,  0,225,  0,227,228,229,230,231,232,233,  0},
+            {  0,236,237,  0,  0,240,  0,242,  0,  0,  0,  0,  0,248,  0,  0,  0},
+            {  0,  0,254,255,  0,257,  0,259,  0,261,262,263,  0,265,266,267,  0},
+            {  0,270,  0,272,273,274,  0,276,277,278,  0,280,281,282,  0,284,  0},
+            {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0}
         };
 
     public BuscaLargura() {
@@ -92,35 +97,28 @@ public class BuscaLargura extends VerificarVisitados{
     /**
      * @param args the command line arguments
      */
-    public Object[] BuscaLargura(){
+    public LinhaColuna[] BuscaLargura(){
         Queue<Integer> fila = new LinkedList<>();
         List<Integer> ListaCaminho = new LinkedList<>();
-        LinhaColuna lc[] = new LinhaColuna[110];
+        LinhaColuna lc[] = new LinhaColuna[153];
+        List<Integer> noExpandidos = new LinkedList<>();
         VerificarVisitados ListaVisitado = new VerificarVisitados();
-        /*int cont = 1;
-        
-        for(int i = 0 ; i < 13 ; i++){
-            for(int j = 0 ; j < 17 ; j++){
-                tabuleiro[i][j] = cont++;
-            }
-        }*/
         //origem do boneco
                
-           int lin = (int) (Math.random()*13);
-           int col = (int) (Math.random()*17);
+           int lin = (int) (Math.random()*18);
+           int col = (int) (Math.random()*10);
            
         while(tabuleiro[lin][col] == 0){
-           lin = (int) (Math.random()*13);
-           col = (int) (Math.random()*17);
+           lin = (int) (Math.random()*18);
+           col = (int) (Math.random()*10);
         }
         boneco = tabuleiro[lin][col];
             
-        
-
         //origem do objetivo
         objetivo = tabuleiro[11][16];
         visitados[0]=boneco;
-        for(int i = 0 ; i < 13 ; i++){
+        
+        for(int i = 0 ; i < 18 ; i++){
             for(int j = 0 ; j < 17 ; j++){
                 if(tabuleiro[i][j] < 10){
                     System.out.print("  " + tabuleiro[i][j] + " ");
@@ -138,7 +136,7 @@ public class BuscaLargura extends VerificarVisitados{
         visitados[0] = boneco;
         fila.add(boneco);
         while(fila.size() > 0){
-            for(int i = 0 ; i < 13 ; i++){
+            for(int i = 0 ; i < 18 ; i++){
                 for(int j = 0 ; j < 17 ; j++){
                     //verificando se a posição inicial da lista qual sua referencia no tabuleiro
                     if(tabuleiro[i][j] == fila.peek()){
@@ -150,16 +148,18 @@ public class BuscaLargura extends VerificarVisitados{
                             if(!ListaVisitado.visitado(tabuleiro[i][j-1], visitados)){
                                 visitados[++contadorVisitados] = tabuleiro[i][j-1];
                                 predecessor[contadorVisitados] = fila.peek();
+                                noExpandidos.add(tabuleiro[i][j-1]);
                                 fila.add(tabuleiro[i][j-1]); 
                             }
                         }
                         //verificar extremo de linha maxima
-                        if(i+1 == 13 || tabuleiro[i+1][j] == parede){
+                        if(i+1 == 19 || tabuleiro[i+1][j] == parede){
                             System.out.println("Não expande em baixo");    
                         }else{
                             if(!ListaVisitado.visitado(tabuleiro[i+1][j], visitados)){
                                 visitados[++contadorVisitados] = tabuleiro[i+1][j];
                                 predecessor[contadorVisitados] = fila.peek();
+                                noExpandidos.add(tabuleiro[i+1][j]);
                                 fila.add(tabuleiro[i+1][j]); 
        
                             }
@@ -171,6 +171,7 @@ public class BuscaLargura extends VerificarVisitados{
                             if(!ListaVisitado.visitado(tabuleiro[i][j+1], visitados)){ 
                                 visitados[++contadorVisitados] = tabuleiro[i][j+1]; 
                                 predecessor[contadorVisitados] = fila.peek();
+                                noExpandidos.add(tabuleiro[i][j+1]);
                                 fila.add(tabuleiro[i][j+1]);  
                             }
                         }
@@ -181,6 +182,7 @@ public class BuscaLargura extends VerificarVisitados{
                             if(!ListaVisitado.visitado(tabuleiro[i-1][j], visitados)){
                                 visitados[++contadorVisitados] = tabuleiro[i-1][j];
                                 predecessor[contadorVisitados] = fila.peek();
+                                noExpandidos.add(tabuleiro[i-1][j]);
                                 fila.add(tabuleiro[i-1][j]);  
                             }
                         }
@@ -188,7 +190,6 @@ public class BuscaLargura extends VerificarVisitados{
                 }
             }
             if(fila.peek() == objetivo){
-               // fila.add(objetivo);
                 break;
             }
             //verificando se o ultimo no a ser expandido é o 
@@ -200,13 +201,17 @@ public class BuscaLargura extends VerificarVisitados{
                 System.out.println("Objetivo inalcançável");
                 System.exit(0);
             }
-            System.out.println("Expandir = " + fila.peek());
-            System.out.println(fila);
-            fila.poll();
+            System.out.println("[" + fila.peek() + "]" + "-> Expandiu ->" + noExpandidos);
+            noExpandidos.removeAll(noExpandidos);
+            
+ 
+            System.out.println("Removeu -> [" + fila.poll() + "]");
+            System.out.println("Fila atual -> " + fila + "\n");
             
 
         }
-        System.out.println(fila);
+        
+        System.out.println("\nObjetivo envontrado. Fila final -> " + fila + "\n");
         //Apos gerar os caminhos verifico a lista de visitado e a dos predecessor 
         //e gero uma terceira lista pra gerar o caminho mais rapido até o objetivo
         int contador = 1;
@@ -223,11 +228,11 @@ public class BuscaLargura extends VerificarVisitados{
                 break;
             ListaCaminho.add(0,caminho[a]);
         }
-        System.out.println("\n"+ListaCaminho);
+        System.out.println("Caminho -> "+ListaCaminho);
         contador = 0;
         //pegando os indices do caminho percorrido usando busca em largura na matriz
         for(Integer elemento : ListaCaminho){
-            for(int i = 0 ; i < 13 ; i++){
+            for(int i = 0 ; i < 18 ; i++){
                 for(int j = 0 ; j < 17 ; j++){
                     if(tabuleiro[i][j] == elemento){
                         lc[contador++] = new LinhaColuna(i, j);
